d9c3dfb9f27f1f1eac9c33a3c30d2f9211249f61
moosecanswim <kyle.gallagher.09@gmail.com>
2017-08-09 09:09:24 -0400

    first commit

---
+++ src/main/java/me/moosecanswim/springboot06/Controller/MainController.java
@@ -0,0 +1,29 @@
+package me.moosecanswim.springboot06.Controller;
+
+import me.moosecanswim.springboot06.Song;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/songform")
+    public String loadFormPage(Model model){
+        model.addAttribute("song", new Song());
+        return "songform";
+    }
+    @PostMapping("/songform")
+    public String loadFromPage(@ModelAttribute Song song, BindingResult bindingResult){
+        if(bindingResult.hasErrors()) {
+            String messageToSend = "There was an issue with the employee added.  Please try again";
+            System.out.println("**Here is an issue**"  + bindingResult.toString());
+            return "/songform";
+        }
+        return "confirmsong";
+    }
+
+}
